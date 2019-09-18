2a18a987fcfcd9fc0ef501f4d18aa45d27a2336c
Wilfredo Fermin <wilfredofermin@hotmail.com>
2019-09-17 21:58:57 -0400

    Boton Logout - accion salir

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -215,15 +215,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </router-link>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-power-off"></i>
-              <p>
-                Salir
-                {{-- <span class="right badge badge-danger">Nuevo</span> --}}
-              </p>
-            </a>
+                  <a class="nav-link" href="{{ route('logout') }}"
+                      onclick="event.preventDefault();
+                      document.getElementById('logout-form').submit();">
+                      <i class="nav-icon fas fa-power-off"></i>
+                      <p>
+                            {{ __('Logout') }}
+                      </p>                  
+                    </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
           </li>
-         
+
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
